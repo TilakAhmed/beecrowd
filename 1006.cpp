f9cd5e560994e493735ee0306cce46f844f5570a
TilakAhmed <90475926+TilakAhmed@users.noreply.github.com>
2022-08-04 17:13:15 +0600

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+    cin >> a >> b >> c;
+    double d=(a*2+b*3+c*5)/(2+3+5);
+    cout << fixed<< setprecision(1)<<"MEDIA = " << d<< endl;
+    return 0;
+}
