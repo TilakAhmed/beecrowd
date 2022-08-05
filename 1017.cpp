aff2f04a42bca762a0c4384bad4c0dc5b9888bb4
TilakAhmed <90475926+TilakAhmed@users.noreply.github.com>
2022-08-05 22:34:36 +0600

    Add files via upload

---
+++ 1017.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>>
+using namespace std;
+
+int main()
+{
+    int spent_time;
+    int average_speed;
+    int  spent_fuel = 12;
+    cin>> spent_time >> average_speed;
+    cout<< fixed << setprecision(3) << double(spent_time * average_speed) / spent_fuel <<endl;
+
+}
